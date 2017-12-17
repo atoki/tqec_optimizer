6822a143019366f2831955a36c27d60f2e96492e
Kota <Askt@Akst.local>
2017-12-17 22:25:22 +0900

    add sa relocation

--- tqec_optimizer/relocation/relocation.py
+++ tqec_optimizer/relocation/relocation.py
@@ -1,4 +1,6 @@
 import copy
+import random
+import math
 import sys
 from collections import defaultdict
 
@@ -126,6 +128,105 @@ class Relocation:
             loop_count += 1
 
         return graph
+
+    def __sa_relocation(self, type_, graph):
+        initial_t = 100
+        final_t = 0.91
+        cool_rate = 0.97
+        limit = 100
+
+        module_list, joint_pair_list = ModuleListFactory(graph, type_).create()
+        current_cost = TqecEvaluator(module_list).evaluate()
+        place = SequenceTriple(type_, module_list)
+        p1, p2, p3 = place.build_permutation()
+        t = initial_t
+        while t > final_t:
+            for n in range(limit):
+                np1, np2, np3 = self.__create_neighborhood(type_, module_list, p1, p2, p3)
+                if np1 is None:
+                    continue
+
+                new_cost = TqecEvaluator(module_list).evaluate()
+
+                if self.__should_change(new_cost - current_cost, t):
+                    current_cost = new_cost
+                    p1, p2, p3 = np1, np2, np3
+                else:
+                    module_list = SequenceTriple(type_, p1, (p1, p2, p3)).recalculate_coordinate()
+            t *= cool_rate
+
+        graph = self.__to_graph(module_list)
+        route_pair = TSP(graph, module_list, joint_pair_list).search()
+        RipAndReroute(graph, module_list, route_pair).search()
+
+        return graph
+
+    def __create_neighborhood(self, type_, module_list, p1, p2, p3):
+        np1, np2, np3 = p1[:], p2[:], p3[:]
+
+        strategy = random.randint(1, 2)
+        # swap
+        if strategy == 1:
+            self.__swap(np1, np2, np3)
+        else:
+            self.__shift(np1, np2, np3)
+
+        module_list = SequenceTriple(type_, np1, (np1, np2, np3)).recalculate_coordinate()
+
+        if self.__is_validate(module_list):
+            return np1, np2, np3
+
+        module_list = SequenceTriple(type_, p1, (p1, p2, p3)).recalculate_coordinate()
+        return None, None, None
+
+    @staticmethod
+    def __swap(p1, p2, p3):
+        size = len(p1)
+        s1 = random.randint(0, size - 1)
+        s2 = random.randint(0, size - 1)
+
+        module1, module2 = p1[s1], p1[s2]
+        p1_index1, p1_index2 = p1.index(module1), p1.index(module2)
+        p2_index1, p2_index2 = p2.index(module1), p2.index(module2)
+        p3_index1, p3_index2 = p3.index(module1), p3.index(module2)
+
+        # permutation swap
+        p1[p1_index1], p1[p1_index2] = p1[p1_index2], p1[p1_index1]
+        p2[p2_index1], p2[p2_index2] = p2[p2_index2], p2[p2_index1]
+        p3[p3_index1], p3[p3_index2] = p3[p3_index2], p3[p3_index1]
+
+    @staticmethod
+    def __shift(p1, p2, p3):
+        size = len(p1)
+        index = random.randint(0, size - 1)
+        shift_size1 = random.randint(0, size - 1)
+        shift_size2 = random.randint(0, size - 1)
+        pair = random.randint(1, 3)
+
+        module_ = p1[index]
+        if pair == 1:
+            p1_index, p2_index = p1.index(module_), p2.index(module_)
+            p1_module, p2_module = p1.pop(p1_index), p2.pop(p2_index)
+            p1.insert(p1_index + shift_size1, p1_module)
+            p2.insert(p2_index + shift_size2, p2_module)
+        elif pair == 2:
+            p1_index, p3_index = p1.index(module_), p3.index(module_)
+            p1_module, p3_module = p1.pop(p1_index), p3.pop(p3_index)
+            p1.insert(p1_index + shift_size1, p1_module)
+            p3.insert(p3_index + shift_size2, p3_module)
+        else:
+            p2_index, p3_index = p2.index(module_), p3.index(module_)
+            p2_module, p3_module = p2.pop(p2_index), p3.pop(p3_index)
+            p2.insert(p2_index + shift_size1, p2_module)
+            p3.insert(p3_index + shift_size2, p3_module)
+
+    @staticmethod
+    def __should_change(delta, t):
+        if delta <= 0:
+            return 1
+        if random.random() < math.exp(- delta / t):
+            return 1
+        return 0
 
     @staticmethod
     def __is_validate(module_list):
@@ -256,4 +357,3 @@ class Relocation:
         print("--- module list ---")
         for module_ in self._module_list:
             module_.debug()
-
