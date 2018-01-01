4f6157bcff1120578f660d87ecbaf80605b0d44d
Kota <Askt@Akst.local>
2018-01-02 01:09:31 +0900

    refactoring

--- tqec_optimizer/relocation/relocation.py
+++ tqec_optimizer/relocation/relocation.py
@@ -79,143 +79,39 @@ class Relocation:
 
         current_cost = TqecEvaluator(module_list).evaluate()
         place = SequenceTriple(module_list)
-        p1, p2, p3 = place.build_permutation()
+        place.build_permutation()
         t = initial_t
-        init = False
         start = time.time()
         while t > final_t:
-            if t % 10 < 1.0:
-                print(t)
+            print(t)
             for n in range(limit):
-                np1, np2, np3 = self.__create_neighborhood(module_list, p1, p2, p3, init, cross_id_set)
-                if np1 is None:
+                place.create_neighborhood()
+                candidate = place.recalculate_coordinate()
+
+                if not self.__is_validate(candidate, cross_id_set):
+                    place.recover()
                     continue
 
-                new_cost = TqecEvaluator(module_list).evaluate()
-
-                # if init and self.__is_validate(module_list, cross_id_set):
-                #     p1, p2, p3 = np1, np2, np3
-                #     t = initial_t
-                #     init = False
+                new_cost = TqecEvaluator(candidate).evaluate()
 
                 if self.__should_change(new_cost - current_cost, t):
                     current_cost = new_cost
-                    p1, p2, p3 = np1[:], np2[:], np3[:]
+                    place.apply()
                 else:
-                    module_list = SequenceTriple(p1, (p1, p2, p3)).recalculate_coordinate()
+                    place.recover()
             t *= cool_rate
 
         elapsed_time = time.time() - start
         print("処理時間: {}".format(elapsed_time))
 
-        if self.__is_validate(module_list, cross_id_set, True):
-            print("Trueですよ")
-
-        self.__color_cross_edge(module_list)
-        graph = self.__to_graph(module_list)
+        result = place.result()
+        self.__color_cross_edge(result)
+        graph = self.__to_graph(result)
         CircuitWriter(graph).write("4-relocation.json")
-        route_pair = TSP(graph, module_list).search()
-        Routing(graph, module_list, route_pair).execute()
+        route_pair = TSP(graph, result).search()
+        Routing(graph, result, route_pair).execute()
 
         return graph
-
-    def __create_neighborhood(self, module_list, p1, p2, p3, init, cross_id_set):
-        """
-        SA用の近傍を生成する
-        1. swap近傍
-        2. shift近傍
-        3. rotate近傍
-        以上の3つを等確率で一つ採用
-        :param module_list モジュールの集合
-        :param p1 順列1
-        :param p2 順列2
-        :param p3 順列3
-        :param init 初期配置が決定していればTrue, そうでなければFalse
-        """
-        np1, np2, np3 = p1[:], p2[:], p3[:]
-
-        strategy = random.randint(1, 3)
-        # swap
-        index, rotate = 0, None
-        if strategy == 1:
-            self.__swap(np1, np2, np3)
-        elif strategy == 2:
-            self.__shift(np1, np2, np3)
-        else:
-            index, rotate = self.__rotate(np1)
-
-        module_list = SequenceTriple(np1, (np1, np2, np3)).recalculate_coordinate()
-
-        if init or self.__is_validate(module_list, cross_id_set):
-            return np1, np2, np3
-
-        module_list = SequenceTriple(p1, (p1, p2, p3)).recalculate_coordinate()
-        if rotate is not None:
-            p1[index].rotate(rotate)
-            p1[index].rotate(rotate)
-            p1[index].rotate(rotate)
-
-        return None, None, None
-
-    @staticmethod
-    def __swap(p1, p2, p3):
-        size = len(p1)
-        s1 = random.randint(0, size - 1)
-        s2 = random.randint(0, size - 1)
-
-        module1, module2 = p1[s1], p1[s2]
-        p1_index1, p1_index2 = p1.index(module1), p1.index(module2)
-        p2_index1, p2_index2 = p2.index(module1), p2.index(module2)
-        p3_index1, p3_index2 = p3.index(module1), p3.index(module2)
-
-        # permutation swap
-        p1[p1_index1], p1[p1_index2] = p1[p1_index2], p1[p1_index1]
-        p2[p2_index1], p2[p2_index2] = p2[p2_index2], p2[p2_index1]
-        p3[p3_index1], p3[p3_index2] = p3[p3_index2], p3[p3_index1]
-
-    @staticmethod
-    def __shift(p1, p2, p3):
-        size = len(p1)
-        index = random.randint(0, size - 1)
-        shift_size1 = random.randint(0, size - 1)
-        shift_size2 = random.randint(0, size - 1)
-        pair = random.randint(1, 3)
-
-        module_ = p1[index]
-        if pair == 1:
-            p1_index, p2_index = p1.index(module_), p2.index(module_)
-            p1_module, p2_module = p1.pop(p1_index), p2.pop(p2_index)
-            p1.insert(p1_index + shift_size1, p1_module)
-            p2.insert(p2_index + shift_size2, p2_module)
-        elif pair == 2:
-            p1_index, p3_index = p1.index(module_), p3.index(module_)
-            p1_module, p3_module = p1.pop(p1_index), p3.pop(p3_index)
-            p1.insert(p1_index + shift_size1, p1_module)
-            p3.insert(p3_index + shift_size2, p3_module)
-        else:
-            p2_index, p3_index = p2.index(module_), p3.index(module_)
-            p2_module, p3_module = p2.pop(p2_index), p3.pop(p3_index)
-            p2.insert(p2_index + shift_size1, p2_module)
-            p3.insert(p3_index + shift_size2, p3_module)
-
-    @staticmethod
-    def __rotate(p1):
-        size = len(p1)
-        index = random.randint(0, size - 1)
-        axis = random.randint(1, 3)
-        rotate_module = p1[index]
-
-        if axis == 1:
-            if rotate_module.rotate('X'):
-                return index, 'X'
-        elif axis == 2:
-            if rotate_module.rotate('Y'):
-                return index, 'Y'
-        else:
-            if rotate_module.rotate('Z'):
-                return index, 'Z'
-
-        return index, None
 
     @staticmethod
     def __should_change(delta, t):
@@ -226,9 +122,7 @@ class Relocation:
         return 0
 
     @staticmethod
-    def __is_validate(module_list, cross_id_set, debug=False):
-        if debug:
-            print("debug on")
+    def __is_validate(module_list, cross_id_set):
         edge_map = {}
         connect_edge = defaultdict(list)
         for module_ in module_list:
@@ -237,12 +131,16 @@ class Relocation:
                 edge = joint_pair[2]
 
                 if joint1 in edge_map and joint2 in edge_map:
+                    connect_edge[edge_map[joint1]].extend(connect_edge[edge_map[joint2]])
+                    connect_edge[edge_map[joint1]].append(edge)
+                    del_edge = edge_map[joint2]
+
                     for edge in connect_edge[edge_map[joint2]]:
                         node1, node2 = edge.node1, edge.node2
                         edge_map[node1] = edge_map[joint1]
                         edge_map[node2] = edge_map[joint1]
-                    connect_edge[edge_map[joint1]].extend(connect_edge[edge_map[joint2]])
-                    del connect_edge[edge_map[joint2]]
+
+                    del connect_edge[del_edge]
 
                 elif joint1 in edge_map:
                     edge_map[joint2] = edge_map[joint1]
@@ -258,28 +156,15 @@ class Relocation:
                     connect_edge[edge].append(edge)
 
         id_set = dict(cross_id_set)
-        if debug:
-            print("--------------")
-            for key_edge, edge_list in sorted(connect_edge.items(), key=lambda x: len(x[1]), reverse=True):
-                print("--")
-                for edge in edge_list:
-                    edge.debug()
-                print("")
         for key_edge, edge_list in sorted(connect_edge.items(), key=lambda x: len(x[1]), reverse=True):
             if len(edge_list) == 1:
                 break
             result = id_set[edge_list[0].module_id]
             for edge in edge_list:
                 tmp = id_set[edge.module_id]
-                if debug:
-                    print("tmp: {}".format(tmp))
                 result = result & tmp
 
-            if debug:
-                print("result: {}".format(result))
             if len(result) == 0:
-                if debug:
-                    print("False !!!!!!!!!!!!")
                 return False
 
             del_num = result.pop()
