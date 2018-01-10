f4467d875b0b1cac7c91b63a65b79e8146dbe81d
Ubuntu16.04 <atok.asai@gmail.com>
2018-01-10 19:00:18 +0900

    delete graph evaluate

--- tqec_optimizer/relocation/tqec_evaluator.py
+++ tqec_optimizer/relocation/tqec_evaluator.py
@@ -2,47 +2,25 @@ import math
 
 
 class TqecEvaluator:
-    def __init__(self, module_list, graph=None, is_graph=False):
+    def __init__(self, module_list):
         self._module_list = module_list
-        self._graph = graph
-        self._is_graph = is_graph
 
     def evaluate(self):
-        point = 0
-        if self._is_graph:
-            point += self.__evaluate_graph()
-
-            return point
-        else:
-            min_x = min_y = min_z = math.inf
-            max_x = max_y = max_z = -math.inf
-            for module_ in self._module_list:
-                for node in module_.cross_node_list:
-                    min_x = min(node.x, min_x)
-                    min_y = min(node.y, min_y)
-                    min_z = min(node.z, min_z)
-                    max_x = max(node.x, max_x)
-                    max_y = max(node.y, max_y)
-                    max_z = max(node.z, max_z)
-
-            width = (max_x - min_x) / 2 + 1
-            height = (max_y - min_y) / 2 + 1
-            depth = (max_z - min_z) / 2 + 1
-
-            point = width + height + depth
-
-            return point
-
-    def __evaluate_graph(self):
         min_x = min_y = min_z = math.inf
         max_x = max_y = max_z = -math.inf
-        for node in self._graph.node_list:
-            if node.type == "primal":
-                min_x, min_y, min_z = min(node.x, min_x), min(node.y, min_y), min(node.z, min_z)
-                max_x, max_y, max_z = max(node.x, max_x), max(node.y, max_y), max(node.z, max_z)
+        for module_ in self._module_list:
+            for node in module_.cross_node_list:
+                min_x = min(node.x, min_x)
+                min_y = min(node.y, min_y)
+                min_z = min(node.z, min_z)
+                max_x = max(node.x, max_x)
+                max_y = max(node.y, max_y)
+                max_z = max(node.z, max_z)
 
         width = (max_x - min_x) / 2 + 1
         height = (max_y - min_y) / 2 + 1
         depth = (max_z - min_z) / 2 + 1
 
-        return width * height * depth
+        point = width + height + depth
+
+        return point
