f1d6006d4e8e0f37f34b8efa847a742ae3e9a35b
Kota <Askt@Akst.local>
2018-11-24 02:25:07 +0900

    update

--- data/circuit_generator.py
+++ data/circuit_generator.py
@@ -16,15 +16,50 @@ Options:
 
 
 from docopt import docopt
+import random
+import json
 
 
 def main():
     args = docopt(__doc__)
-    n_qubit = args['--qubit'][0]
-    n_gate = args['--gate'][0]
+    n_qubit = int(args['--qubit'][0])
+    n_gate = int(args['--gate'][0])
     output_file = args['--output'][0]
 
-    
+    print("{}qubit  {}gates".format(n_qubit, n_gate))
+
+    qubit_list = [n for n in range(0, n_qubit)]
+    output_list = [n for n in range(0, n_qubit)]
+    init_list = []
+    meas_list = []
+    cnot_list = []
+
+    for n in range(0, n_qubit):
+        basis = "x" if random.randint(0, 1) == 0 else "z"
+        dict = {"bit": n,
+                "type": basis}
+        init_list.append(dict)
+
+    for n in range(0, n_gate):
+        control = random.randint(0, n_qubit - 1)
+        target = control
+        while target == control:
+            target = random.randint(0, n_qubit - 1)
+        target_list = [target]
+        dict = {"type": "cnot",
+                "control": control,
+                "targets": target_list}
+        cnot_list.append(dict)
+
+    data = {"bits": qubit_list,
+            "outputs": output_list,
+            "initializations": init_list,
+            "measurements": meas_list,
+            "operations": cnot_list}
+
+    with open(output_file, 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+
 
 if __name__ == '__main__':
     main()
